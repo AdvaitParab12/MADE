d66dbf1d7d3315f257ad30e2cbf7e6590f3337f9
Advait Parab <advait0012@proton.me>
2025-02-04 08:02:27 +0000

    messages page ui done

---
+++ client/components/index.js
@@ -0,0 +1,11 @@
+import SignUp from './SignUp'
+import Messages from './Messages'
+import Chats from './Chats'
+import Inputs from './Inputs'
+
+export {
+  SignUp,
+  Messages,
+  Chats,
+  Inputs
+}
