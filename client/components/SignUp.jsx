333d1021bde136773296c897190db6f1cdc9ab99
Advait Parab <advait0012@proton.me>
2025-02-10 07:56:26 +0000

    add loading

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Card,
   CardHeader,
@@ -9,74 +10,89 @@ import {
   Form,
   Input,
   Button,
+  Spinner,
 } from "@heroui/react";
-import { useEffect } from "react";
 
 function SignUp({ setUser, socket }) {
+  const [isLoading, setisLoading] = useState(true);
+
+  // Check from session storage
   useEffect(() => {
+    setisLoading(false);
     const session = sessionStorage.getItem("user");
     if (session) {
       setUser(session);
     }
-  });
+  }, []);
+
   const onSubmit = (e) => {
+    // Prevent default browser page refresh.
     e.preventDefault();
 
+    // Get form data as an object.
     const data = Object.fromEntries(new FormData(e.currentTarget));
 
-    setUser(data.name);
+    // Set into session
+    sessionStorage.setItem("user", data.name);
+
+    // Emit to the server
     socket.emit("user", data.name);
 
-    sessionStorage.setItem("user", data.name);
+    // Update parent's state
+    setUser(data.name);
   };
 
   return (
-    <div className="flex items-center justify-center bg-blue-200 min-h-screen">
-      <Card className="max-w-[400px]">
-        <CardHeader className="flex gap-3">
-          <Image
-            alt="made logo"
-            height={40}
-            radius="sm"
-            src="favicon.ico"
-            width={40}
-          />
-          <div className="flex flex-col">
-            <p className="text-md">MADE Room</p>
-            <p className="text-small text-default-500">made.phleebs.tech</p>
-          </div>
-        </CardHeader>
-
-        <Divider />
-
-        <CardBody>
-          <Form onSubmit={onSubmit} validationBehavior="native">
-            <Input
-              isRequired
-              errorMessage="Please enter a valid name"
-              label="Name"
-              labelPlacement="outside"
-              name="name"
-              placeholder="Enter your name"
-              type="text"
-              autoComplete="off"
+    <div className="flex items-center justify-center  bg-blue-200 min-h-screen">
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="made logo"
+              height={40}
+              radius="sm"
+              src="favicon.ico"
+              width={40}
             />
-            <Button type="submit">Enter</Button>
-          </Form>
-        </CardBody>
+            <div className="flex flex-col">
+              <p className="text-md">MADE Room</p>
+              <p className="text-small text-default-500">made.phleebs.tech</p>
+            </div>
+          </CardHeader>
 
-        <Divider />
+          <Divider />
 
-        <CardFooter>
-          <Link
-            isExternal
-            showAnchorIcon
-            href="https://github.com/gliese-sketch/made"
-          >
-            Visit source code on GitHub.
-          </Link>
-        </CardFooter>
-      </Card>
+          <CardBody>
+            <Form onSubmit={onSubmit} validationBehavior="native">
+              <Input
+                isRequired
+                errorMessage="Please enter a valid name"
+                label="Name"
+                labelPlacement="outside"
+                name="name"
+                placeholder="Enter your name"
+                type="text"
+                autoComplete="off"
+              />
+              <Button type="submit">Enter</Button>
+            </Form>
+          </CardBody>
+
+          <Divider />
+
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://github.com/gliese-sketch/made"
+            >
+              Visit source code on GitHub.
+            </Link>
+          </CardFooter>
+        </Card>
+      )}
     </div>
   );
 }
