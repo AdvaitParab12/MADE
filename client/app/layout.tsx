16c6e58402ab3681d9bae780653c789f45220041
Advait Parab <advait0012@proton.me>
2025-02-10 07:20:27 +0000

    add ai funtionality

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
 const inter = Inter({
   variable: "--font-inter-sans",
   subsets: ["latin"],
@@ -20,11 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        {children}
-      </body>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        ></meta>
+      </head>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
